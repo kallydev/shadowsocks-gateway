d1c6b5d00b456dacfa1edc7795a9c1db7f65c216
kallydev <kallydev@gmail.com>
2021-09-08 15:52:22 +0800

    Replace fmt package with log

--- example/main.go
+++ example/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/google/uuid"
 	gateway "github.com/kallydev/shadowsocks-gateway"
 	"github.com/kallydev/shadowsocks-gateway/config"
@@ -19,15 +18,15 @@ func main() {
 		Remote: "127.0.0.1",
 	})
 	instance.Event.ConnectHandler = func(ctx context.Context, id uuid.UUID, network string, remote net.Addr) bool {
-		fmt.Println("connect", ctx, id, network, remote)
+		log.Println("connect", ctx, id, network, remote)
 		return true
 	}
 	instance.Event.ForwardHandler = func(ctx context.Context, id uuid.UUID, direction string, size int) bool {
-		fmt.Println("forward", ctx, id, direction, size)
+		log.Println("forward", ctx, id, direction, size)
 		return true
 	}
 	instance.Event.CloseHandler = func(ctx context.Context, id uuid.UUID) {
-		fmt.Println("close", id)
+		log.Println("close", id)
 	}
 	if err := instance.Run(); err != nil {
 		log.Fatalln(err)
--- go.mod
+++ go.mod
@@ -2,12 +2,4 @@ module github.com/kallydev/shadowsocks-gateway
 
 go 1.17
 
-require (
-	github.com/go-yaml/yaml v2.1.0+incompatible
-	github.com/google/uuid v1.3.0
-)
-
-require (
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
+require github.com/google/uuid v1.3.0
